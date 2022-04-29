5bcf86b0762897aacaf2f1cc8e906729e142347f
PN <pn@klapproth-koch.de>
2022-04-29 10:32:26 +0200

    Initial commit

---
+++ files/assets/css/footer.php
@@ -0,0 +1,14 @@
+<div class="footer">
+    <div class="footer-top">
+        {{insert_article::16}}
+    </div>
+    <div class="footer-middle">
+        {{insert_module::2}}
+        {{insert_link::11}}  {{insert_link::12}}
+        {{insert_article::17}}
+    </div>
+    <div class="footer-bottom">
+        {{insert_article::18}}
+    </div>
+
+</div>
