e4914153b746dd78605a17fddefcade9e252dfc8
PN <pn@klapproth-koch.de>
2022-05-04 09:09:12 +0200

    neue-styling nach dem Inhalt

---
+++ templates/menu.php
@@ -0,0 +1,72 @@
+<nav class="mod_navigation">
+    <ul class="menu-list">
+        <li>
+            <div>
+                {{insert_module::25}}
+            </div>
+
+        </li>
+
+        <li>
+            <section class="ce_accordionSingle ce_accordion ce_text block">
+                <div class="toggler ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons " role="tab" id="ui-id-1" aria-controls="ui-id-2" tabindex="0">
+                    <span>Kopf-Hals-Tumoren</span></div>
+                <div class="accordion ui-accordion-content ui-corner-bottom ui-helper-reset " style="display: block;" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false">
+                    <div>
+                        {{insert_module::26}}
+                    </div>
+                </div>
+            </section>
+        </li>
+
+        <li>
+            <section class="ce_accordionSingle ce_accordion ce_text block">
+                <div class="toggler ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons " role="tab" id="ui-id-1" aria-controls="ui-id-2" tabindex="0">
+                    <span>Leben mit Kopf-Hals-Krebs</span></div>
+                <div class="accordion ui-accordion-content ui-corner-bottom ui-helper-reset " style="display: block;" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false">
+                    <div>
+                        {{insert_module::27}}
+                    </div>
+                </div>
+            </section>
+        </li>
+        <li>
+            <div>
+                {{insert_module::28}}
+            </div>
+        </li>
+        <li>
+            <div>
+                {{insert_module::29}}
+            </div>
+        </li>
+
+        <li>
+            <section class="ce_accordionSingle ce_accordion ce_text block">
+                <div class="toggler ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons " role="tab" id="ui-id-1" aria-controls="ui-id-2" tabindex="0">
+                    <span>Die Make-Sense-Kampagne</span></div>
+                <div class="accordion ui-accordion-content ui-corner-bottom ui-helper-reset " style="display: block;" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false">
+                    <div>
+                        {{insert_module::30}}
+                    </div>
+                </div>
+            </section>
+        </li>
+
+        <li>
+
+            <section class="ce_accordionSingle ce_accordion ce_text block">
+                <div class="toggler ui-accordion-header ui-corner-top ui-state-default ui-accordion-icons " role="tab" id="ui-id-1" aria-controls="ui-id-2" tabindex="0">
+                    <span>Profil</span></div>
+                <div class="accordion ui-accordion-content ui-corner-bottom ui-helper-reset " style="display: block;" id="ui-id-2" aria-labelledby="ui-id-1" role="tabpanel" aria-hidden="false">
+                    <div>
+                        {{insert_module::31}}
+                    </div>
+                </div>
+            </section>
+        </li>
+        <li>
+            <div>
+
+    </ul>
+</nav>
